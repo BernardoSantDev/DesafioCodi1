c9b0246e730e5734b5f246a043743c6ff8cb274d
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-03-11 14:47:57 -0300

    adiciona o evento do click

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const toggleBtn = document.getElementById('toggleBtn');
-const sensitiveElements = document.querySelectorAll('.sensitive-data')
+const sensitiveElements = document.querySelectorAll('.sensitive-data');
+let isHidden = false;
+
+toggleBtn.addEventListener('click', () => {
 
 
 
+
+    
+})
 
 
 
